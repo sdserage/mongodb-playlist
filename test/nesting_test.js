febfd7ec801163c7e9e5cdd14e63593fa9f1555c
Scott Serage <xttocsurge51@gmail.com>
2018-08-31 11:29:26 -0600

    Added a test for adding to anexisting author

--- test/nesting_test.js
+++ test/nesting_test.js
@@ -4,6 +4,13 @@ const Author = require('../models/author');
 
 // Describe our tests
 describe('Nesting records', function(){
+
+  this.beforeEach(function(done){
+    mongoose.connection.collections.authors.drop(function(){
+      done();
+    });
+  });
+
   // Create tests
   it('Create an author with sub-documents', function(done){
     let pat = new Author({
@@ -14,6 +21,25 @@ describe('Nesting records', function(){
       Author.findOne({name: 'Patrick Rothfuss'}).then(function(result) {
         assert(result.books.length === 1);
         done();
+      });
+    });
+  });
+
+  it('Add a book to an author', function(done){
+    let pat = new Author({
+      name: 'Patrick Rothfuss',
+      books: [{title: 'Name of the Wind', pages: 400}],
+    });
+    pat.save().then(function(){
+      Author.findOne({name: 'Patrick Rothfuss'}).then(function(result) {
+        // add a book to the books array
+        result.books.push({title: "Wise Man's Fear", pages: 500});
+        result.save().then(function() {
+          Author.findOne({name: 'Patrick Rothfuss'}).then(function(result){
+            assert(result.books.length === 2);
+            done();
+          });
+        });
       });
     });
   });
